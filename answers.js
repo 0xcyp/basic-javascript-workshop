7104dcd8ec9d0f78b1dbc612088e8c4ddf91c1d0
vntrp <grau.cyprien@gmail.com>
2015-09-11 19:09:36 +0000

    fixed every return instead of console.log

--- answers.js
+++ answers.js
@@ -2,14 +2,14 @@
 // Test your function on a few inputs, including the empty string.
 
 function firstCharacter(string0) { 
-    console.log(string0.charAt(0)) 
+    return string0.charAt(0) 
 }
 
 // Write a function that takes a string and returns the last character of a string.
 // Test your function on a few inputs, including the empty string.
 
 function lastCharacter(string1) {
-    console.log(string1.slice(-1));
+    return string1.slice(-1)
 }
 
 // Write a function that takes a string and a number,
@@ -17,23 +17,21 @@ function lastCharacter(string1) {
 // The indexing of number should start at 0. Test your function on a few inputs, including the empty string.
 
 function characterPosition(string2, number0) {
-    console.log(string2.charAt(number0))
+    return string2.charAt(number0)
 }
 
 // Write a function that takes two numbers and adds them together. Test your function on a few inputs.
 // Write in the comments what happens when you pass something other than a number to your function.
 
 function addNumbers(number1, number2){
-    var localVariable=number1+number2
-    console.log(localVariable)
+    return number1+number2
 }                                       // when other thing than number, the fonction concatenates (the strings for example)
 
 // Write a function that takes two numbers and multiplies them together. Test your function on a few inputs.
 // Write in your comments what happens when you pass something other than a number to your function.
 
 function multiplyNumbers(number3, number4){
-    var localVariable=number3*number4
-    console.log(localVariable)
+    return number3*number4
 }                                       // when other thing than number, get a "NaN"
 
 // Write a function that takes two numbers and a string. If the string is ‘add’, then return the sum of the numbers.
@@ -67,23 +65,23 @@ function multiplyNumbers(number3, number4){
 function doAsISayWithNumbers(number5, number6, string2) {
     switch(string2) {
     case "add":
-        var localVariable1=number5+number6
-        console.log(localVariable1)
+        return number5+number6
         break;
     case "substract":
-        var localVariable2=number5-number6
-        console.log(localVariable2)
+        return number5-number6
         break;
     case "mult":
-        var localVariable3=number5*number6
-        console.log(localVariable3)
+        return number5*number6
         break;
     case "div":
-        var localVariable4=number5/number6
-        console.log(localVariable4)
+        return number5/number6
         break;
     default:
         console.log(0)
     }
 }
 
+// Write a function that takes a string and a number, and returns the string repeated that many number of times.
+// Test your function with various inputs.
+
+function repeatedString(string3,number7){
