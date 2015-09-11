880daed6db7345e4346396b8b2aebd2d1b2b3a01
vntrp <grau.cyprien@gmail.com>
2015-09-11 19:18:53 +0000

    fixed a bunch of stuff as recommended

--- answers.js
+++ answers.js
@@ -1,23 +1,23 @@
 // Write a function that takes a string and returns the first character of the string.
 // Test your function on a few inputs, including the empty string.
 
-function firstCharacter(string0) { 
-    return string0.charAt(0) 
+function firstCharacter(strg) { 
+    return strg.charAt(0) 
 }
 
 // Write a function that takes a string and returns the last character of a string.
 // Test your function on a few inputs, including the empty string.
 
-function lastCharacter(string1) {
-    return string1.slice(-1)
+function lastCharacter(strg) {
+    return strg.slice(-1)
 }
 
 // Write a function that takes a string and a number,
 // and returns the character at the position represented by the number.
 // The indexing of number should start at 0. Test your function on a few inputs, including the empty string.
 
-function characterPosition(string2, number0) {
-    return string2.charAt(number0)
+function characterPosition(strg, number) {
+    return strg.charAt(number)
 }
 
 // Write a function that takes two numbers and adds them together. Test your function on a few inputs.
@@ -30,8 +30,8 @@ function addNumbers(number1, number2){
 // Write a function that takes two numbers and multiplies them together. Test your function on a few inputs.
 // Write in your comments what happens when you pass something other than a number to your function.
 
-function multiplyNumbers(number3, number4){
-    return number3*number4
+function multiplyNumbers(number1, number2){
+    return number1*number2
 }                                       // when other thing than number, get a "NaN"
 
 // Write a function that takes two numbers and a string. If the string is ‘add’, then return the sum of the numbers.
@@ -60,22 +60,18 @@ function multiplyNumbers(number3, number4){
     }
 } */
 
-// long and boring function...
+// long and boring function... I prefer switch:
 
-function doAsISayWithNumbers(number5, number6, string2) {
-    switch(string2) {
+function doAsISayWithNumbers(number1, number2, strg) {
+    switch(strg) {
     case "add":
-        return number5+number6
-        break;
+        return number1+number2
     case "substract":
-        return number5-number6
-        break;
+        return number1-number2
     case "mult":
-        return number5*number6
-        break;
+        return number1*number2
     case "div":
-        return number5/number6
-        break;
+        return number1/number2
     default:
         console.log(0)
     }
@@ -84,4 +80,6 @@ function doAsISayWithNumbers(number5, number6, string2) {
 // Write a function that takes a string and a number, and returns the string repeated that many number of times.
 // Test your function with various inputs.
 
-function repeatedString(string3,number7){
+function repeatedString(strg,number) {
+    
+}
