99767f8fb70d76a032a3e3715364767717c97b35
vntrp <grau.cyprien@gmail.com>
2015-09-11 18:35:47 +0000

    addNumbers

--- answers.js
+++ answers.js
@@ -20,3 +20,12 @@ function characterPosition(string, number) {
     console.log(string.charAt(number))
 }
 
+// Write a function that takes two numbers and adds them together.
+// Test your function on a few inputs.
+// Write in the comments what happens when you pass something other than a number to your function.
+
+function addNumbers(number1, number2){
+    var localVariable=number1+number2
+    console.log(localVariable)
+}                                       // when other thing than number, the fonction concatenates (the strings for example)
+
