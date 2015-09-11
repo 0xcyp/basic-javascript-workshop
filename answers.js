6cb7580ead56943dbf41aeb24df6a93b2d2b221f
vntrp <grau.cyprien@gmail.com>
2015-09-11 18:21:53 +0000

    lastCharacter

---
+++ answers.js
@@ -0,0 +1,14 @@
+// Write a function that takes a string and returns the first character of the string.
+// Test your function on a few inputs, including the empty string.
+
+function firstCharacter(string) { 
+    console.log(string.charAt(0)) 
+}
+
+// Write a function that takes a string and returns the last character of a string.
+// Test your function on a few inputs, including the empty string.
+
+function lastCharacter(string) {
+    console.log(string.slice(-1));
+}
+
