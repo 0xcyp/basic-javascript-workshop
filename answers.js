6597d5550febc7d308af5c1781a7d3ee82c07302
vntrp <grau.cyprien@gmail.com>
2015-09-11 18:38:13 +0000

    multiplyNumbers

--- answers.js
+++ answers.js
@@ -20,12 +20,19 @@ function characterPosition(string, number) {
     console.log(string.charAt(number))
 }
 
-// Write a function that takes two numbers and adds them together.
-// Test your function on a few inputs.
+// Write a function that takes two numbers and adds them together. Test your function on a few inputs.
 // Write in the comments what happens when you pass something other than a number to your function.
 
 function addNumbers(number1, number2){
     var localVariable=number1+number2
     console.log(localVariable)
 }                                       // when other thing than number, the fonction concatenates (the strings for example)
+
+// Write a function that takes two numbers and multiplies them together. Test your function on a few inputs.
+// Write in your comments what happens when you pass something other than a number to your function.
+
+function multiplyNumbers(number1, number2){
+    var localVariable=number1*number2
+    console.log(localVariable)
+}                                       // when other thing than number, get a "NaN"
 
